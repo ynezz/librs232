b3aed3bad7ffe2656f781975a03fcbff51a7a0ce
Petr Stetiar <ynezz@true.cz>
2009-07-14 10:02:29 +0200

    correct legal entity for Gaben

--- src/rs232_windows.c
+++ src/rs232_windows.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 Petr Stetiar <ynezz@true.cz>, Gaben
+ * Copyright (c) 2009 Petr Stetiar <ynezz@true.cz>, Gaben Ltd.
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
