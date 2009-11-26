dcafdfe314979a7030f5783d7cd9f47dba4503e1
Petr Štetiar <ynezz@true.cz>
2009-11-26 16:51:56 +0100

    add 2400 and 4800 baudrates to Linux also

--- src/rs232_linux.c
+++ src/rs232_linux.c
@@ -481,6 +481,14 @@ rs232_set_baud(struct rs232_port_t *p, enum rs232_baud_e baud)
 	GET_PORT_STATE(ux->fd, &term)
 
 	switch (baud) {
+	case RS232_BAUD_2400:
+		cfsetispeed(&term, B2400);
+		cfsetospeed(&term, B2400);
+		break;
+	case RS232_BAUD_4800:
+		cfsetispeed(&term, B4800);
+		cfsetospeed(&term, B4800);
+		break;
 	case RS232_BAUD_9600:
 		cfsetispeed(&term, B9600);
 		cfsetospeed(&term, B9600);
