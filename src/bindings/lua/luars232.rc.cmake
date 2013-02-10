8b10873f0ee5243df9e8a117b76ac2a2d2357995
Petr Štetiar <ynezz@true.cz>
2013-02-10 22:41:51 +0100

    Reorganize the directory layout to better suit CMake
    * make the pkg-config's and Win RC files CMake templating ready
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- src/bindings/lua/luars232.rc.cmake
+++ src/bindings/lua/luars232.rc.cmake
@@ -1,8 +1,8 @@
 #include "afxres.h"
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 1,0,3,0
- PRODUCTVERSION 1,0,3,0
+FILEVERSION ${LIB_VERSION_MAJOR},${LIB_VERSION_MINOR},${LIB_VERSION_PATCH},${LIB_VERSION_LUA}
+ PRODUCTVERSION ${LIB_VERSION_MAJOR},${LIB_VERSION_MINOR},${LIB_VERSION_PATCH},${LIB_VERSION_LUA}
  FILEFLAGSMASK 0x17L
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -19,12 +19,12 @@ BEGIN
         BEGIN
             VALUE "CompanyName", "Petr Stetiar, Gaben Ltd.\0"
             VALUE "FileDescription", "luars232 - Lua library for serial communication over RS232\0"
-            VALUE "FileVersion", "1, 0, 3, 0\0"
+	    VALUE "FileVersion", "${LIB_VERSION_MAJOR},${LIB_VERSION_MINOR},${LIB_VERSION_PATCH},${LIB_VERSION_LUA}\0"
             VALUE "InternalName", "luars232\0"
             VALUE "LegalCopyright", "Copyright (c) 2012 Petr Stetiar, Gaben Ltd.\0"
             VALUE "OriginalFilename", "luars232.dll\0"
             VALUE "ProductName", "luars232 - Library for serial communication over RS232\0"
-            VALUE "ProductVersion", "1, 0, 3, 0\0"
+	    VALUE "ProductVersion", "${LIB_VERSION_MAJOR},${LIB_VERSION_MINOR},${LIB_VERSION_PATCH},${LIB_VERSION_LUA}\0"
         END
     END
     BLOCK "VarFileInfo"
---
+++ src/librs232.pc.cmake
@@ -0,0 +1,5 @@
+Name: ${PROJECT_NAME}
+Description: Library for serial communication over RS232
+Version: @LIBRARY_VERSION@
+Libs: -L${LIB_INSTALL_DIR} -lrs232
+Cflags: -I${INCLUDE_INSTALL_DIR}
