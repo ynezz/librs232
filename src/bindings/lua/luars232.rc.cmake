f7b4fc3485325cb4f9cbc11b8dcbc9035174d997
Petr Štetiar <ynezz@true.cz>
2013-02-10 22:52:30 +0100

    Update copyright year to something more accurate
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- src/bindings/lua/luars232.rc.cmake
+++ src/bindings/lua/luars232.rc.cmake
@@ -21,7 +21,7 @@ BEGIN
             VALUE "FileDescription", "luars232 - Lua library for serial communication over RS232\0"
 	    VALUE "FileVersion", "${LIB_VERSION_MAJOR},${LIB_VERSION_MINOR},${LIB_VERSION_PATCH},${LIB_VERSION_LUA}\0"
             VALUE "InternalName", "luars232\0"
-            VALUE "LegalCopyright", "Copyright (c) 2012 Petr Stetiar, Gaben Ltd.\0"
+            VALUE "LegalCopyright", "Copyright (c) 2013 Petr Stetiar, Gaben Ltd.\0"
             VALUE "OriginalFilename", "luars232.dll\0"
             VALUE "ProductName", "luars232 - Library for serial communication over RS232\0"
 	    VALUE "ProductVersion", "${LIB_VERSION_MAJOR},${LIB_VERSION_MINOR},${LIB_VERSION_PATCH},${LIB_VERSION_LUA}\0"
