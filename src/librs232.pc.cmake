1f2cd17c562d8b3447183caafb6546fbcdf162d5
Petr Štetiar <ynezz@true.cz>
2013-03-19 14:00:09 +0100

    Add project URL into the pkg-config file
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- src/librs232.pc.cmake
+++ src/librs232.pc.cmake
@@ -1,5 +1,6 @@
 Name: ${PROJECT_NAME}
 Description: Library for serial communication over RS232
+URL: http://github.com/ynezz/librs232/
 Version: @LIBRARY_VERSION@
 Libs: -L${LIB_INSTALL_DIR} -lrs232
 Cflags: -I${INCLUDE_INSTALL_DIR}
