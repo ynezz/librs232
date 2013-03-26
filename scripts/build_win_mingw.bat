673f8e707ede360d71f0e00e5daf8925f5f4c813
Petr Stetiar <ynezz@true.cz>
2013-03-26 16:03:27 +0100

    Fix the cmocka test-suite on Windows/VS2010
    Signed-off-by: Petr Stetiar <ynezz@true.cz>

--- scripts/build_win_mingw.bat
+++ scripts/build_win_mingw.bat
@@ -7,7 +7,7 @@ del /f /s /q build\mingw
 mkdir build\mingw
 cd build\mingw
 
-cmake -G "MinGW Makefiles" -DLUA_BINDINGS:bool=on -DLUA_LIBRARY:string="h:\Lua\5.1\lib\lua51.lib" -DLUA_INCLUDE_DIR:string="h:\Lua\5.1\include" ..\..
+cmake -G "MinGW Makefiles" -DUNIT_TESTING=on -DLUA_BINDINGS:bool=on -DCMOCKA_BIN_DIR:path="d:/projekty/vc/cmocka/vs2010/bin" -DCMOCKA_INCLUDE_DIR:path="d:\projekty\vc\cmocka\vs2010\include" -DCMOCKA_LIBRARY="d:\projekty\vc\cmocka\vs2010\lib\cmocka.lib" -DLUA_LIBRARY:filepath="h:\Lua\5.1\lib\lua51.lib" -DLUA_INCLUDE_DIR:path="h:\Lua\5.1\include" ..\..
 
 make package package_source
 cd ..\..
--- scripts/build_win_vs2k10.bat
+++ scripts/build_win_vs2k10.bat
@@ -2,12 +2,12 @@
 set CMAKE_BIN="d:\MinGW\CMake 2.8\bin"
 set PATH=%CMAKE_BIN%;c:\windows\system32;%PATH%
 
-del /f /s /q build\vs2k10
+del /f /s /q build\vs2k10 > NUL
 mkdir build\vs2k10
 cd build\vs2k10
 
 @call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-cmake -G "NMake Makefiles" -DLUA_BINDINGS:bool=on -DLUA_LIBRARY:string="h:\Lua\5.1\lib\lua51.lib" -DLUA_INCLUDE_DIR:string="h:\Lua\5.1\include" ..\..
+cmake -G "NMake Makefiles" -DUNIT_TESTING=on -DLUA_BINDINGS:bool=on -DCMOCKA_BIN_DIR:path="d:/projekty/vc/cmocka/vs2010/bin" -DCMOCKA_INCLUDE_DIR:path="d:\projekty\vc\cmocka\vs2010\include" -DCMOCKA_LIBRARY="d:\projekty\vc\cmocka\vs2010\lib\cmocka.lib" -DLUA_LIBRARY:filepath="h:\Lua\5.1\lib\lua51.lib" -DLUA_INCLUDE_DIR:path="h:\Lua\5.1\include" ..\..
 
-nmake package package_source
+nmake all test package package_source
 cd ..\..
