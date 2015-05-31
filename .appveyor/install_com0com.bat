3eca8b73003d39271adc964b96d17cafd7c89225
Alexey Melnichuk <mimir@newmail.ru>
2015-05-31 16:02:19 +0400

    Install signed version of com0com

--- .appveyor/install_com0com.bat
+++ .appveyor/install_com0com.bat
@@ -7,19 +7,25 @@ if not exist %COM0COM_ROOT%\setupc.exe (
   echo ======================================================
   echo Download com0com
   echo ======================================================
-  curl --output tt.zip --silent --fail --max-time 120 --connect-timeout 30 -L %COM0COM_URL%
-  7z x -aoa tt.zip
-  rm tt.zip
+  curl --silent --fail --max-time 120 --connect-timeout 30 -L --output com0com.cer https://github.com/hybridgroup/rubyserial/raw/appveyor_deps/com0com.cer
+  curl --silent --fail --max-time 120 --connect-timeout 30 -L --output setup_com0com_W7_x64_signed.exe https://github.com/hybridgroup/rubyserial/raw/appveyor_deps/setup_com0com_W7_x64_signed.exe
+  :: curl --output tt.zip --silent --fail --max-time 120 --connect-timeout 30 -L %COM0COM_URL%
+  :: 7z x -aoa tt.zip
+  :: rm tt.zip
   echo ======================================================
   echo Install com0com
   echo ======================================================
-  cd %COM0COM_BASE%
-  .\setup.exe /S /D=%COM0COM_ROOT%
-  dir %COM0COM_ROOT%
+  CertMgr /add com0com.cer /s /r localMachine root
+  CertMgr /add com0com.cer /s /r localMachine trustedpublisher
+  setup_com0com_W7_x64_signed.exe /S /D=%COM0COM_ROOT%
+  :: cd %COM0COM_BASE%
+  :: .\setup.exe /S /D=%COM0COM_ROOT%
+  :: dir %COM0COM_ROOT%
   echo ======================================================
   echo install virtual serial ports
   echo ======================================================
   cd %COM0COM_ROOT%
   .\setupc.exe --silent --detail-prms install 0 - -
   .\setupc.exe --silent --detail-prms install 1 - -
+  sleep 60
 )
