05624c72ec9a5c2b37081771434acc7cbbe31047
Alexey Melnichuk <mimir@newmail.ru>
2015-05-31 13:00:35 +0400

    Install Virtual ports and run test

--- .appveyor/install_com0com.bat
+++ .appveyor/install_com0com.bat
@@ -4,8 +4,17 @@ set COM0COM_BASE=com0com-3.0.0.0-i386-and-x64
 set COM0COM_URL=http://downloads.sourceforge.net/project/com0com/com0com/3.0.0.0/%COM0COM_BASE%-unsigned.zip
 
 if not exist %COM0COM_ROOT%\setupc.exe (
+  echo ======================================================
+  echo Download com0com
+  echo ======================================================
   curl --output tt.zip --silent --fail --max-time 120 --connect-timeout 30 -L %COM0COM_URL%
   7z x -aoa tt.zip
   rm tt.zip
   cd %COM0COM_BASE% && .\setup.exe /S %COM0COM_ROOT%
+  cd %COM0COM_ROOT%
+  echo ======================================================
+  echo install virtual serial ports
+  echo ======================================================
+  ./setupc --silent --detail-prms install 0 - -
+  ./setupc --silent --detail-prms install 1 - -
 )
